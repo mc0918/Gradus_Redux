f6834505b1b034f95765cba3a9f7ee45275a7225
mc0918 <mcampbell0918@gmail.com>
2019-05-06 10:54:52 -0500

    CORS troubleshooting

--- server.js
+++ server.js
@@ -20,16 +20,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-var databaseURI = "mongodb://localhost/Gradus";
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-} else {
-  mongoose.connect(databaseURI, { useNewUrlParser: true });
-}
-var db = mongoose.connection;
-db.on("error", err => console.log("mongoose error :", err));
-db.once("open", () => console.log("mongoose connection successful"));
-
 app.use(function(req, res, next) {
   // Website you wish to allow to
   //either localhost:3000 or heroku deployed link
@@ -57,6 +47,16 @@ app.use(function(req, res, next) {
   // Pass to next layer of middleware
   next();
 });
+
+var databaseURI = "mongodb://localhost/Gradus";
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+} else {
+  mongoose.connect(databaseURI, { useNewUrlParser: true });
+}
+var db = mongoose.connection;
+db.on("error", err => console.log("mongoose error :", err));
+db.once("open", () => console.log("mongoose connection successful"));
 
 app.use(passport.initialize());
 require("./passport")(passport);
