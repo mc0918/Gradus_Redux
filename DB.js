8774b1a4b2da4b44600570a6c024cb7799dfe6a5
mc0918 <mcampbell0918@gmail.com>
2019-05-05 23:11:02 -0500

    added mlab connectivity

--- DB.js
+++ DB.js
@@ -3,7 +3,12 @@
 //To deploy on heroku both a local and MLAB database need to be exported
 
 module.exports = {
-  // the /axios is the name of the database
-  //DB: process.env.MONGO
-  DB: "mongodb://localhost:27017/axios"
+  // axios is the name of the local database
+  DB: "mongodb://localhost:27017/axios",
+
+  //mongodb is not used in the react app but run in the command line to set a config variable
+  //that we can access in node.js as process.env.MONGO
+  //gradus and gradus123 are database login (user, pass respectively)
+  //take this variable out of final build
+  mongodb: "gradus:gradus123@ds151626.mlab.com:51626/heroku_9z5q0j31"
 };
--- server.js
+++ server.js
@@ -4,24 +4,39 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/userRoute");
 const path = require("path");
+
 //DB connectivity
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
 const PORT = process.env.PORT || 4000;
 //const PORT = 4000;
 const cors = require("cors");
 const config = require("./DB");
 
-//Need to change this for Heroku I think
-mongoose.connect(config.DB).then(
-  () => {
-    console.log("DB connected");
-  },
-  err => {
-    console.log("connection err: " + err);
+var db;
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
   }
-);
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+});
+
+//Need to change this for Heroku I think
+// mongoose.connect(config.DB).then(
+//   () => {
+//     console.log("DB connected");
+//   },
+//   err => {
+//     console.log("connection err: " + err);
+//   }
+// );
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,6 +55,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //Start server
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log("Server is running on Port: ", PORT);
 });
