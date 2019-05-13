5c130a9d581c09c344f98f734902713766305621
mc0918 <mcampbell0918@gmail.com>
2019-05-13 13:58:06 -0500

    running db test on server.js

--- server.js
+++ server.js
@@ -22,7 +22,96 @@ if (process.env.MONGODB_URI) {
 }
 var db = mongoose.connection;
 db.on("error", err => console.log("mongoose error :", err));
-db.once("open", () => console.log("mongoose connection successful"));
+// db.once("open", () => console.log("mongoose connection successful"));
+
+//======================================================================================TEST=======================
+db.once("open", function callback() {
+  // Create song schema
+  let songSchema = mongoose.Schema({
+    decade: String,
+    artist: String,
+    song: String,
+    weeksAtOne: Number
+  });
+
+  // Store song documents in a collection called "songs"
+  let Song = mongoose.model("songs", songSchema);
+
+  // Create seed data
+  let seventies = new Song({
+    decade: "1970s",
+    artist: "Debby Boone",
+    song: "You Light Up My Life",
+    weeksAtOne: 10
+  });
+
+  let eighties = new Song({
+    decade: "1980s",
+    artist: "Olivia Newton-John",
+    song: "Physical",
+    weeksAtOne: 10
+  });
+
+  let nineties = new Song({
+    decade: "1990s",
+    artist: "Mariah Carey",
+    song: "One Sweet Day",
+    weeksAtOne: 16
+  });
+
+  /*
+   * First we'll add a few songs. Nothing is required to create the
+   * songs collection; it is created automatically when we insert.
+   */
+
+  let list = [seventies, eighties, nineties];
+
+  Song.insertMany(list)
+    .then(() => {
+      /*
+       * Then we need to give Boyz II Men credit for their contribution
+       * to the hit "One Sweet Day".
+       */
+
+      return Song.update(
+        { song: "One Sweet Day" },
+        { $set: { artist: "Mariah Carey ft. Boyz II Men" } }
+      );
+    })
+    .then(() => {
+      /*
+       * Finally we run a query which returns all the hits that spend 10 or
+       * more weeks at number 1.
+       */
+
+      return Song.find({ weeksAtOne: { $gte: 10 } }).sort({ decade: 1 });
+    })
+    .then(docs => {
+      docs.forEach(doc => {
+        console.log(
+          "In the " +
+            doc["decade"] +
+            ", " +
+            doc["song"] +
+            " by " +
+            doc["artist"] +
+            " topped the charts for " +
+            doc["weeksAtOne"] +
+            " straight weeks."
+        );
+      });
+    })
+    .then(() => {
+      // Only close the connection when your app is terminating
+      return mongoose.connection.close();
+    })
+    .catch(err => {
+      // Log any errors that are thrown in the Promise chain
+      console.log(err);
+    });
+});
+
+//======================================================================================TEST=======================
 
 app.use(passport.initialize());
 require("./passport")(passport);
@@ -71,7 +160,6 @@ app.use("/api/users", users);
 //NEW STUFF HERE
 const cantus = require("./serverRoutes/api/database");
 app.use("/api/cantus", cantus);
-console.log(cantus);
 
 // var databaseURI = "mongodb://localhost/Gradus";
 // if (process.env.MONGODB_URI) {
--- serverRoutes/api/database.js
+++ serverRoutes/api/database.js
@@ -1,11 +1,13 @@
 // const mongoose = require('mongoose')
 let databaseController = require("../../serverController/methods_DB");
+var Cantus = require("../../serverModels/CantusFirmus");
 const router = require("express").Router();
 // mongoose.connect("mongodb://localhost/Gradus", { useNewUrlParser: true });
 
 //Changed "/" to "/cantus"
 //Changed.route to .get
 router.get("/cantus").get(databaseController.findAll, (req, res) => {
+  console.log("RES ", res);
   return res.json(res);
 });
 // router.get("/", )
