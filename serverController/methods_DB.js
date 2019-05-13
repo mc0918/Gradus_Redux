5ec122026ec35ff4c491165a766765fb1c59aba7
mc0918 <mcampbell0918@gmail.com>
2019-05-13 00:03:48 -0500

    testing db connection

--- serverController/methods_DB.js
+++ serverController/methods_DB.js
@@ -1,15 +1,13 @@
 //Put functions that routes call in here... ex: db.findAll() etc.
-let db = require("../serverModels")
+let db = require("../serverModels");
 //controller should only export functions to query the DB
 
 module.exports = {
-    findAll: function (req, res) {
-        db.CantusFirmus.find({}).then(
-            data => {
-                res.json(data)
-                console.log("data: ", data)
-            }
-        )
-    }
-
-}
+  findAll: function(req, res) {
+    db.CantusFirmus.find({}).then(data => {
+      // res.json(data)
+      res.json("clam");
+      console.log("data: ", data);
+    });
+  }
+};
