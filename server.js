9f392bd4dc37ff60fbb985cdec5d448c4096255b
mc0918 <mcampbell0918@gmail.com>
2019-05-13 14:51:47 -0500

    testing connected routes

--- server.js
+++ server.js
@@ -101,10 +101,10 @@ db.once("open", function callback() {
         );
       });
     })
-    .then(() => {
-      // Only close the connection when your app is terminating
-      return mongoose.connection.close();
-    })
+    // .then(() => {
+    //   // Only close the connection when your app is terminating
+    //   return mongoose.connection.close();
+    // })
     .catch(err => {
       // Log any errors that are thrown in the Promise chain
       console.log(err);
@@ -159,7 +159,11 @@ app.use("/api/users", users);
 
 //NEW STUFF HERE
 const cantus = require("./serverRoutes/api/database");
-app.use("/api/cantus", cantus);
+app.get("/api/database", (req, res) => {
+  Song.find({}).toArray(function(err, docs) {
+    res.send(docs);
+  });
+});
 
 // var databaseURI = "mongodb://localhost/Gradus";
 // if (process.env.MONGODB_URI) {
