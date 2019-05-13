e2c11473895f557fbf1d24e0ef011b4a88d4bacd
mc0918 <mcampbell0918@gmail.com>
2019-05-13 15:16:57 -0500

    still fixing ye olde api routes

--- server.js
+++ server.js
@@ -64,117 +64,118 @@ db.once("open", function callback() {
    * songs collection; it is created automatically when we insert.
    */
 
-  let list = [seventies, eighties, nineties];
+  // let list = [seventies, eighties, nineties];
 
-  Song.insertMany(list)
-    .then(() => {
-      /*
-       * Then we need to give Boyz II Men credit for their contribution
-       * to the hit "One Sweet Day".
-       */
+  // Song.insertMany(list)
+  //   .then(() => {
+  //     /*
+  //      * Then we need to give Boyz II Men credit for their contribution
+  //      * to the hit "One Sweet Day".
+  //      */
 
-      return Song.update(
-        { song: "One Sweet Day" },
-        { $set: { artist: "Mariah Carey ft. Boyz II Men" } }
-      );
-    })
-    .then(() => {
-      /*
-       * Finally we run a query which returns all the hits that spend 10 or
-       * more weeks at number 1.
-       */
+  //     return Song.update(
+  //       { song: "One Sweet Day" },
+  //       { $set: { artist: "Mariah Carey ft. Boyz II Men" } }
+  //     );
+  //   })
+  //   .then(() => {
+  /*
+   * Finally we run a query which returns all the hits that spend 10 or
+   * more weeks at number 1.
+   */
 
-      return Song.find({ weeksAtOne: { $gte: 10 } }).sort({ decade: 1 });
-    })
-    .then(docs => {
-      docs.forEach(doc => {
-        console.log(
-          "In the " +
-            doc["decade"] +
-            ", " +
-            doc["song"] +
-            " by " +
-            doc["artist"] +
-            " topped the charts for " +
-            doc["weeksAtOne"] +
-            " straight weeks."
-        );
-      });
-    })
-    // .then(() => {
-    //   // Only close the connection when your app is terminating
-    //   return mongoose.connection.close();
-    // })
-    .catch(err => {
-      // Log any errors that are thrown in the Promise chain
-      console.log(err);
+  //   return Song.find({ weeksAtOne: { $gte: 10 } }).sort({ decade: 1 });
+  // })
+  // .then(docs => {
+  //   docs.forEach(doc => {
+  //     console.log(
+  //       "In the " +
+  //         doc["decade"] +
+  //         ", " +
+  //         doc["song"] +
+  //         " by " +
+  //         doc["artist"] +
+  //         " topped the charts for " +
+  //         doc["weeksAtOne"] +
+  //         " straight weeks."
+  //     );
+  //   });
+  // })
+  // .then(() => {
+  //   // Only close the connection when your app is terminating
+  //   return mongoose.connection.close();
+  // })
+  //     .catch(err => {
+  //       // Log any errors that are thrown in the Promise chain
+  //       console.log(err);
+  //     });
+  // });
+
+  //======================================================================================TEST=======================
+
+  app.use(passport.initialize());
+  require("./passport")(passport);
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  //=====DB/login dependencies
+  if (process.env.NODE_ENV === "production") {
+    // Exprees will serve up production assets
+    app.use(express.static("client/build"));
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require("path");
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
-});
+  }
 
-//======================================================================================TEST=======================
+  app.use(function(req, res, next) {
+    // Website you wish to allow to
+    //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
+    res.setHeader("Access-Control-Allow-Origin", "*");
 
-app.use(passport.initialize());
-require("./passport")(passport);
+    // Request methods you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-//=====DB/login dependencies
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("client/build"));
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    // Request headers you wish to allow
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    // Pass to next layer of middleware
+    next();
   });
-}
 
-app.use(function(req, res, next) {
-  // Website you wish to allow to
-  //either localhost:3000 or heroku deployed link (https://guarded-sands-13025.herokuapp.com)
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  app.use("/", routes);
+  app.use("/api/users", users);
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
-app.use("/", routes);
-app.use("/api/users", users);
-
-//NEW STUFF HERE
-const cantus = require("./serverRoutes/api/database");
-app.get("/api/database", (req, res) => {
-  Song.find({}).toArray(function(err, docs) {
-    res.send(docs);
+  //NEW STUFF HERE
+  const cantus = require("./serverRoutes/api/database");
+  app.post("/api/database", (req, res) => {
+    Song.find({}).toArray(function(err, docs) {
+      res.send(docs);
+      console.log("DOOOOOOOOCCCCSSSS!!!", docs);
+    });
   });
+
+  // var databaseURI = "mongodb://localhost/Gradus";
+  // if (process.env.MONGODB_URI) {
+  //   mongoose.connect(process.env.MONGODB_URI);
+  // } else {
+  //   mongoose.connect(databaseURI);
+  // }
+  // var db = mongoose.connection;
+  // db.on("error", err => console.log("mongoose error :", err));
+  // db.once("open", () => console.log("mongoose connection successful"));
 });
-
-// var databaseURI = "mongodb://localhost/Gradus";
-// if (process.env.MONGODB_URI) {
-//   mongoose.connect(process.env.MONGODB_URI);
-// } else {
-//   mongoose.connect(databaseURI);
-// }
-// var db = mongoose.connection;
-// db.on("error", err => console.log("mongoose error :", err));
-// db.once("open", () => console.log("mongoose connection successful"));
-
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
